0c6f511ebd1c79b2ad74f22ce63d940ebf6c7cd0
ruona <ogheneruonaagadagba4@gmail.com>
2023-05-26 11:09:47 +0100

    worked on the home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,36 +1,55 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import {useGetUserID} from "../hooks/useGetUserId"
+
 export const Home = () => {
-  const [recipes, setRecipies] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const userId = useGetUserID();
+
   useEffect(() => {
     const fetchRecipe = async () => {
       try {
         const response = await axios.get("http://localhost:8000/recipies");
-        setRecipies(response.data);
-        console.log(response.data[0].name)
+        setRecipes(response.data);
       } catch (err) {
         console.log(err);
       }
     };
+
     fetchRecipe();
   }, []);
-  if(!recipes) return null
+
+  if (!recipes) return null;
+
+  const saveRecipe = async (recipeId) => {
+    try {
+      const response = await axios.put("http://localhost:8000/recipies", {
+        recipeId,
+        userId,
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
-      <h2>Recipies</h2>
+      <h2>Recipes</h2>
       <ul>
-        {recipes.map((recipe, i) => {
+        {recipes.map((recipe, i) => (
           <li key={i}>
             <div>
               <h3>{recipe.name}</h3>
+              <button onClick={() => saveRecipe(recipe._id)}>Save</button>
             </div>
-            {/* <div className="instructions">
+            <div className="instructions">
               <p>{recipe.instructions}</p>
             </div>
             <img src={recipe.imageUrl} alt={recipe.name} />
-            <p>Cooking Time: {recipe.cookingTime} (min)</p> */}
-          </li>;
-        })}
+            <p>Cooking Time: {recipe.cookingTime} (min)</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- server/src/server.js
+++ server/src/server.js
@@ -2,33 +2,34 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-
-import {userRouter} from "./routes/users.js"
-import {recipeRouter} from "./routes/recipies.js"
+import { userRouter } from "./routes/users.js";
+import { recipeRouter } from "./routes/recipies.js";
 
 const app = express();
-  
+
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", userRouter)
-app.use("/recipies", recipeRouter)
- 
+app.use("/auth", userRouter);
+app.use("/recipies", recipeRouter);
 
-mongoose.connect(`mongodb+srv://ruona:${process.env.PASSWORD}@receipes.lmcmfss.mongodb.net/?retryWrites=true&w=majority`).then(() => {
-        console.log("Connected to database!");
-        app.listen(8000, () => { 
-            console.log("server is running on port 8000");
-        }); 
-    }) 
-    .catch((error) => {
-        console.log(error);
-    }); 
- 
+mongoose
+  .connect(
+    `mongodb+srv://ruona:${process.env.PASSWORD}@receipes.lmcmfss.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(8000, () => {
+      console.log("server is running on port 8000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // app.listen(8000, () => {
 //     console.log("listening to port 8000")
